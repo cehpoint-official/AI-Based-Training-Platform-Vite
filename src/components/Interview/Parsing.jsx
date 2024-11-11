147b287ecb122b2b5bdd8f892c6db86b9940558e
S_A_M <108063107+SAMAKSH10@users.noreply.github.com>
2024-11-11 16:55:10 +0530

    update in Virtual Interview

--- src/components/Interview/Parsing.jsx
+++ src/components/Interview/Parsing.jsx
@@ -636,7 +636,8 @@ const ResumeUpload = ({ onUploadComplete }) => {
                   : "bg-indigo-600 hover:bg-indigo-700"
               } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
               disabled={
-                isUploading || isParsing || !selectedFile || !meetsCriteria
+                isUploading || isParsing || !selectedFile  || !meetsCriteria
+               
               }
             >
               {isUploading || isParsing ? (
