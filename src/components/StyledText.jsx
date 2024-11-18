ec573a7e1ad7ff74ad13a56a36e6b1cb83ccaac3
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2024-11-18 19:09:58 +0530

    modified

--- src/components/StyledText.jsx
+++ src/components/StyledText.jsx
@@ -7,7 +7,7 @@ const StyledText = ({
   aiExplanation,
   handleAIGeneratedExplanation,
   isLoading,
-  type
+  type,
 }) => {
   const aiSectionRef = useRef(null);
 
@@ -25,23 +25,26 @@ const StyledText = ({
     <div className="w-full">
       {/* Scroll Trigger */}
       {type === "video & text course" && (
-        <div className="text-center my-4 flex items-center justify-center">
-        <p>If you're not satisfied with the video notes,</p>
-        <button
-          onClick={scrollToAISection}
-          className="text-red-500 underline hover:text-red-600 cursor-pointer ml-1"
-        >
-           try generating text notes!
-        </button>
-      </div>
-      )}      
+        <div className="text-center my-4 flex items-center justify-center max-lg:flex-col ">
+          <p>If you're not satisfied with the video notes,</p>
+          <button
+            onClick={scrollToAISection}
+            className="text-red-500 underline hover:text-red-600 cursor-pointer ml-1"
+          >
+            try generating text notes!
+          </button>
+        </div>
+      )}
 
       {/* Transcript Section */}
-      <div className="w-full flex items-center justify-center flex-col">
-        <div className="w-full bg-white/40 h-[1px]"></div>
-        <h2 className="text-center w-full">VIDEO NOTES</h2>
-        <div className="w-full bg-white/40 h-[1px]"></div>
-      </div>
+      {type === "video & text course" && (
+        <div className="w-full flex items-center justify-center flex-col">
+          <div className="w-full bg-white/40 h-[1px]"></div>
+          <h2 className="text-center w-full">VIDEO NOTES</h2>
+          <div className="w-full bg-white/40 h-[1px]"></div>
+        </div>
+      )}
+
       <div
         className="text-black dark:text-white"
         dangerouslySetInnerHTML={{ __html: text }}
@@ -51,36 +54,33 @@ const StyledText = ({
 
       {type === "video & text course" && (
         <section id="generate-ai-section" ref={aiSectionRef} className="mt-10">
-        <div className="w-full flex items-center justify-center">
-          {aiExplanation ? (
-            <div className="w-full flex items-start justify-start flex-col">
-              <div className="w-full flex items-center justify-center flex-col">
-                <div className="w-full bg-white/40 h-[1px]"></div>
-                <h2 className="text-center w-full">
-                  GENERATED TEXT NOTES
-                </h2>
-                <div className="w-full bg-white/40 h-[1px]"></div>
+          <div className="w-full flex items-center justify-center">
+            {aiExplanation ? (
+              <div className="w-full flex items-start justify-start flex-col">
+                <div className="w-full flex items-center justify-center flex-col">
+                  <div className="w-full bg-white/40 h-[1px]"></div>
+                  <h2 className="text-center w-full">GENERATED TEXT NOTES</h2>
+                  <div className="w-full bg-white/40 h-[1px]"></div>
+                </div>
+                <div
+                  id="aiExplanationContainer"
+                  dangerouslySetInnerHTML={{
+                    __html: aiExplanation,
+                  }}
+                />
               </div>
-              <div
-                id="aiExplanationContainer"
-                dangerouslySetInnerHTML={{
-                  __html: aiExplanation,
-                }}
-              />
-            </div>
-          ) : (
-            <button
-              className="bg-green-700 w-[16rem] py-3 text-white rounded-lg hover:bg-green-800"
-              onClick={handleAIGeneratedExplanation}
-              disabled={isLoading}
-            >
-              {isLoading ? "Generating..." : "Generate AI Explanation"}
-            </button>
-          )}
-        </div>
-      </section>
+            ) : (
+              <button
+                className="bg-green-700 w-[16rem] py-3 text-white rounded-lg hover:bg-green-800"
+                onClick={handleAIGeneratedExplanation}
+                disabled={isLoading}
+              >
+                {isLoading ? "Generating..." : "Generate AI Explanation"}
+              </button>
+            )}
+          </div>
+        </section>
       )}
-      
     </div>
   );
 };
